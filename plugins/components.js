afbd9d0102a040248f4ade9f8b80c9f5c19b9026
xuess <wuniu2010@126.com>
2018-05-16 16:25:01 +0800

    package

--- plugins/components.js
+++ plugins/components.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Toast from '../components/Toast.vue'
 import iView from 'iview'
 import { Header, Content, Table, Button, Layout, Sider, Menu, MenuItem, Icon ,Row ,Col } from 'iview'
-
+//iview 组件
 Vue.use(iView)
 
 const components = {
